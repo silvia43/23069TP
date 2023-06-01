224cb5624fa7c22a9e5db6103a207a59a36db38a
silvia castelo <simacas@hotmail.com>
2023-06-01 01:19:58 -0300

    archivos nuevos y modificados

--- resumen.js
+++ resumen.js
@@ -1,24 +1,56 @@
-const ticket = [
-    {
-        precio: 200,
-        tipo: [
-            'estudiante',
-            'trainee',
-            'junior'
-        ]
-    },
-]
-const descuento= [
-    '80%', '50%', '15%'
 
-]
+function ing_entradas() {
+    return document.getElementById('cant_entradas').value;
 
-
-function ing_entradas(){
-    let entradas = document.getElementById('cant_entradas');
-    let totalEntradas = entradas.value;
-    return(totalEntradas); 
-    totalEntradas = 20;
-    
 }
-console.log(totalEntradas);
+
+function opciones() {
+    let categoria = document.getElementById('categoria');
+    let opciones = categoria.options[categoria.selectedIndex];
+
+    return opciones.value;
+}
+
+
+function descuento() {
+    let categoria = opciones();
+    let porcentaje;
+    switch (categoria) {
+        case '1':
+            porcentaje = 0.80;
+            break;
+        case '2':
+            porcentaje = 0.50;
+            break;
+        case '3':
+            porcentaje = 0.15;
+            break;
+    }
+    let resultado = 1 - porcentaje;
+    return Math.round(resultado * 100) / 100;
+}
+
+function importe() {
+    const precio = 200;
+    const cant = parseInt(ing_entradas());
+    return cant * parseInt(precio);
+}
+function aplicarDescuento() {
+    return importe() * descuento();
+}
+
+const resumen = () => {
+    document.getElementById("resultado").innerHTML = `Total a pagar: $ ${aplicarDescuento()}`
+}
+
+
+function borrar(){
+    document.getElementById("resultado").innerHTML = "";
+    document.getElementById('cant_entradas').value= "";
+
+
+}
+   
+
+
+
